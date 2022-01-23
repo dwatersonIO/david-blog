d0b46afb88e4712e8647ee306a02d70a4a920983
dwatersonIO <dwaterson68@gmail.com>
2022-01-23 20:22:39 +0100

    fixed prob

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import { mdsvex } from "mdsvex";
 import mdsvexConfig from "./mdsvex.config.js";
-// import adapter from "@sveltejs/adapter-auto";
-import adapter from "@sveltejs/adapter-netlify";
+import adapter from "@sveltejs/adapter-auto";
+// import adapter from "@sveltejs/adapter-netlify";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
