f6e4e360674e84429ca2bdb902fdeae630556331
dwatersonIO <dwaterson68@gmail.com>
2022-01-28 13:27:33 +0100

    improved formatting of blogs

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,6 @@
 <script>
     import Nav from "$lib/Nav.svelte"
+    import '../styles/global.css'
 </script>
 
 <div class="container">
@@ -7,22 +8,3 @@
     <slot/>
 </div>
 
-
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300&family=Roboto:wght@300;400&display=swap');
-
-:root {
-    font-family: 'Roboto', sans-serif;
-    --primary: red;
-    --secondary: blue;
-
-}
-
-.container {
-    max-width: 70%;
-    margin: 0em auto;
-    background-color: rgba(117, 115, 115, 0.219);
-    }
-
-
-</style>
---
+++ src/routes/blog/git-general.md
@@ -0,0 +1,24 @@
+---
+title: How to Use Git & GitHub
+summary: New git projects then push to github
+tags: 
+    - tech
+    - git
+date: 2022-01-20
+---
+
+# {title}
+
+## First time
+
+- git add .  
+- git commit -m "my changes"  
+- git remote add origin:  
+`(https://github.com/GitHub username/GitHuB repo-name.git)`
+- git push -u origin master  
+
+## To later update
+
+- git add .  
+- git commit -m "my other changes"  
+- git push  
